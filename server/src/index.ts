c1e403bf3513adf595a2cd1d4885421d7c6b9ae4
aradovic23 <radovic.aleksa@gmail.com>
2022-11-15 16:56:36 +0100

    Displaying Decks on the Landing Page

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,11 +3,13 @@ config();
 
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import Deck from "./models/Deck";
 
 const PORT = 8000;
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.post("/decks", async (req: Request, res: Response) => {
@@ -16,6 +18,11 @@ app.post("/decks", async (req: Request, res: Response) => {
   });
   const createdDeck = await newDeck.save();
   res.json(createdDeck);
+});
+
+app.get("/decks", async (req: Request, res: Response) => {
+  const decks = await Deck.find();
+  res.json(decks);
 });
 
 mongoose.connect(process.env.MONGO_URL!).then(() => {
