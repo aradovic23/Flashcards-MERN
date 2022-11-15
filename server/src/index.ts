befab445b53f3c92ceeec7d69e81e6300d9bd602
Aleksa Radovic <aleksaradovic@Aleksas-MacBook-Air.local>
2022-11-15 16:07:40 +0100

    Inital repo and adding a POST endpoint

---
+++ server/src/index.ts
@@ -0,0 +1,24 @@
+import { config } from "dotenv";
+config();
+
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+
+import Deck from "./models/Deck";
+
+const PORT = 8000;
+const app = express();
+app.use(express.json());
+
+app.post("/decks", async (req: Request, res: Response) => {
+  const newDeck = new Deck({
+    title: req.body.title,
+  });
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
+});
+
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+  console.log(`Listening on port ${PORT}`);
+  app.listen(PORT);
+});
